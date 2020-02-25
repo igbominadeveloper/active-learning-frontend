2d0383ac191547b4b83de356e39843e1522215fa
Favour Afolayan <favourafolayan@gmail.com>
2020-02-25 10:31:31 +0100

    chore(project setup): setup application routing (#4)
    [finishes #171426234]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import LoginPage from './pages/Login';
 
 const App = () => (
-  <Homepage />
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => (
+    <>
+        <Link to="/">Back to homepage</Link>
+        <p>Login Page</p>
+    </>
+);
+
+export default Login;
