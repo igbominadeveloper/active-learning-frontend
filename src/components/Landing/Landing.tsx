e45bebf7069c05a2f94d16238e2c4bba226d9fac
Favour Afolayan <favourafolayan@gmail.com>
2020-02-25 14:36:57 +0100

    chore(landing page): create the navbar
    [starts #171425807]

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
-import logo from '../../logo.svg';
+import NavBar from '../NavBar';
 
 import './Landing.scss';
 
-function App() {
+const Landing = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link to="/login">Login page</Link>
-      </header>
+    <div className="Layout">
+      <Grid columns={16}>
+        <NavBar />
+      </Grid>
     </div>
   );
 }
 
-export default App;
+export default Landing;
