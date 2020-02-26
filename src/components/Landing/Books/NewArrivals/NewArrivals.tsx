ac66b807072da6b6ce55fc485adeb154debf38da
Favour Afolayan <favourafolayan@gmail.com>
2020-02-26 02:38:48 +0100

    feature(about page): build out the store page
    [delivers #171425827]

--- src/components/Landing/Books/NewArrivals/NewArrivals.tsx
+++ src/components/Landing/Books/NewArrivals/NewArrivals.tsx
@@ -1,39 +1,17 @@
 import React from 'react';
-import { Grid, Comment } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
-import BookMock1 from '../../../../assets/images/book-mock.png';
+import Book from '../Book';
 
-const SingleBook = () => (
-    <Comment className="d-flex justify-content-between align-items-center shadow">
-        <div>
-          <Comment.Avatar as="a" src={BookMock1} />
-          <Comment.Text className="text-bold">My new book</Comment.Text>
-        </div>
-        <Comment.Content>
-          <Comment.Author as="p" className="m-0">
-            Matt Daniel
-          </Comment.Author>
-          <Comment.Metadata>
-            <span>Today at 5:42PM</span>
-          </Comment.Metadata>
-          <Comment.Actions className="text-bold">
-            <Link to="">
-              <span>Buy now</span>
-            </Link>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
-);
 
 const NewArrivals = () => (
-  <Grid.Column width={8} className="d-flex flex-column justify-content-center">
+  <Grid.Column width={8} className="d-flex flex-column justify-content-center books-section">
     <h3 className="Books__text-sub mt-0 text-center">New Arrivals</h3>
-    <div className="details d-flex justify-content-between align-items-center flex-wrap">
-      <SingleBook />
-      <SingleBook />
-      <SingleBook />
-      <SingleBook />
+    <div className="details d-flex justify-content-center align-items-center flex-wrap">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </div>
   </Grid.Column>
 );
---
+++ src/pages/Store.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Book from '../components/Landing/Books/Book';
+
+const Store = () => (
+    <div className="Store">
+        <h2 className="text-left">Inventory</h2>
+        <div className="d-flex flex-wrap">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </div>
+    </div>
+);
+
+export default Store;
